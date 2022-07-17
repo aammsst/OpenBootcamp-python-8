6cd4308528e9b25421d70d191fdbfa8a17c6d694
Fabricio Navarro <Navarro.fa998@gmail.com>
2022-07-16 21:14:55 -0300

    corrección

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,20 @@
-# Creamos archivo py ( ejercicio11 )
-f = open('ejercicio11.py','w')
-# Creamos archivo de texto ( texto1 ) y escribimos en él
-f.write('f = open(\'texto1.txt\',\'w\')\nf.write(\'Hola xd\')\nf.close()')
+# Primer intento - Mal interpreté la consigna, pero quedó interesante igual
+# 
+## Creamos archivo py ( ejercicio11 )
+#f = open('ejercicio11.py','w')
+## Creamos archivo de texto ( texto1 ) y escribimos en él
+#f.write('f = open(\'texto1.txt\',\'w\')\nf.write(\'Hola xd\')\nf.close()')
+#
+#f.close()
+## Importamos ejercicio11 para que se ejecute lo que escribimos antes 
+#import ejercicio11
 
-f.close()
-# Importamos ejercicio11 para que se ejecute lo que escribimos antes 
-import ejercicio11
+# Segundo intento después de ver la resolución
+file = open('archivoTxt.txt', 'w')
+file.write('Hola!\n')
+file.close()
+
+file = open('archivoTxt.txt', 'r+')
+file.readline()
+file.write('Segunda vez escribiendo.')
+file.close()
